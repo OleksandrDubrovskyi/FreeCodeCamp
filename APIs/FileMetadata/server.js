868e0cc1db4c5b4c06d0de41c418c377a9ff8497
dubrovsky@live.com <dubrovsky@live.com>
2018-07-08 14:13:22 +0300

    Metadata project

--- APIs/FileMetadata/server.js
+++ APIs/FileMetadata/server.js
@@ -1,21 +1,28 @@
+//https://likeable-jumper.glitch.me/
+
 'use strict';
 
 var express = require('express');
 var cors = require('cors');
-
-// require and use "multer"...
+var multer = require('multer');
+var bodyParser = require('body-parser');
 
 var app = express();
+var upload = multer({dest: 'uploads/'});
 
 app.use(cors());
+app.use(bodyParser.json());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
   });
 
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res, next){
+    res.json({name: req.file.originalname, 
+            type: req.file.mimetype,
+            size: req.file.size
+          });
 });
 
 app.listen(process.env.PORT || 3000, function () {
