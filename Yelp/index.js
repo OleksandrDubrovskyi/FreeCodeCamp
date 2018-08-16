18b00f3bb946e1d46d6a39fd335a5c933f9ee0d9
dubrovsky@live.com <dubrovsky@live.com>
2018-08-16 23:27:03 +0300

    Campgrounds

--- Yelp/index.js
+++ Yelp/index.js
@@ -7,5 +7,15 @@ app.get("/", function(req, res) {
     res.render("landing");
 })
 
+app.get("/campgrounds", function(req, res) {
+    let campgrounds = [
+        {name: "Salmon Creek", image: "https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg"},
+        {name: "Granite Hill", image: "https://pixabay.com/get/e83db40e28fd033ed1584d05fb1d4e97e07ee3d21cac104496f4c27ea7e5b2be_340.jpg"},
+        {name: "Mountain Goat's Rest", image: "https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104496f4c27ea7e5b2be_340.jpg"}
+    ];
+
+    res.render("campgrounds", {campgrounds: campgrounds});
+})
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT);
